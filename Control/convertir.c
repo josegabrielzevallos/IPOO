ffeaf90a3156c91ea03b1b5e09cc345c37a992f3
josegabrielzevallos <jose.zevallos.delgadolaflor@ucsp.edu.pe>
2016-09-16 12:57:58 -0500

    Update convertir.c

--- Control/convertir.c
+++ Control/convertir.c
@@ -1,35 +1,52 @@
-char convertir(char s[])
+#include <stdio.h>
+
+float convertir(char m[])
 {
-    int i;
-    float n;
+    int i,j=1;
+    int a1=0;
+    int a2;
+    float num1=0;
+    float num2=0;
 
-    n=0;
-    int x=0;
 
-
-    for (i=0;x!=1;++i)
+    for(i=0;m[i]>='0'&&m[i]<='9';i++)
     {
-        if (s[i]=='e')
-        {
-            x=1;
-            break;
-        }
-        if (s[i]=='.')
-            continue;
-        n=10*n+(s[i]-'0');
+        num1=10*num1+(m[i]-'0');
+        a1++;//3
     }
+    a1++;//4
+    a2=a1;
 
-    n=n/100000;
-    printf("%f\n",n);
+    for(i=a1;m[i]>='0'&&m[i]<='9';i++)
+    {
+        num2=10*num2+(m[i]-'0');
+        j*=10;
+        a2++;//6
+    }
+    num2=num1+(num2/j);//num2 tiene el numero digital
+    a2++;//7
+    num1=0;
 
+    for(i=a2+1;m[i]>='0'&&m[i]<='9';i++)
+        num1=10*num1+(m[i]-'0');//obtiene el valor exponencial
+    j=1;
+    if(m[a2]=='-')
+    {
+        for(i=0;i<num1;i++)
+            j*=10;
+        return num2/j;
+    }
+    else if(m[a2]=='+')
+    {
+        for(i=0;i<num1;i++)
+            j*=10;
+        return num2*j;
+    }
 }
 
 int main()
 {
-
-    char s[100]="123.45e-6";
-    convertir(s);
-
-    return 0;
+    char x[30]="123.45e-6";
+    printf("Numero: %.10f",convertir(x));
 }
 
