9ca828d02fb69c41360adeb5a2f6854971a45e57
josegabrielzevallos <jose.zevallos.delgadolaflor@ucsp.edu.pe>
2016-09-22 08:16:02 -0500

    Create ejercicio4.c

---
+++ PUNTEROS/ejercicio4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int strend(char *p
+           , char *t)
+{
+    int cont=0;
+    while(*s!='\0')
+        s++;
+    while(*t!='\0')
+    {
+        t++;
+        cont++;
+    }
+    while(cont!=0)
+    {
+        if(*s==*t)
+        {
+            cont--;
+            s--;
+            t--;
+        }
+        else
+            return 0;
+    }
+    return 1;
+}
+
+int main()
+{
+    char s[15]="lunasol",p[15]="sol";
+    strend(s,p);
+}
