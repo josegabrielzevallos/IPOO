1349563a41de887de01d39ca79287eec36b38e99
josegabrielzevallos <jose.zevallos.delgadolaflor@ucsp.edu.pe>
2016-10-28 13:09:23 -0500

    Update main.cpp

--- PUNTO-CLASES/main.cpp
+++ PUNTO-CLASES/main.cpp
@@ -6,27 +6,24 @@ int main()
 {
 
 
-/*
     Point a =  Point(4,5);
     Point b =  Point(5,4);
     Point c =  Point(6,6);
     ArrayPoint temp = ArrayPoint(4);
     Point *tt = new Point[4];
-    // temp6 = temp.getSize();
-    //cout << temp6 << endl;
+     temp6 = temp.getSize();
+    cout << temp6 << endl;
     temp._insert(2,a);
-    //cout << "2"<< endl;
+    cout << "2"<< endl;
     temp._insert(2,b);
-    //temp._insert(2,c);
+    temp._insert(2,c);
     ArrayPoint ty = ArrayPoint(tt,2,4);
     ty.print();
     ArrayPoint doo = temp;
     doo._insert(0,c);
     doo.print();
     temp.print();
-*/
 
-/*
     ArrayPoint temp = ArrayPoint(4);
     Point a =  Point(4,5);
     Point b =  Point(5,4);
@@ -34,16 +31,13 @@ int main()
     temp._insert(0,a);
     temp._insert(1,b);
     temp._insert(2,c);
-    ///temp.print();
-    ///cout << endl;
-    ///temp._remove(1);
-    ///temp.print();
+    temp.print();
+   
+    temp._remove(1);
+    temp.print();
     temp._push_back(c);
     temp.print();
-*/
 
-/*
-  
 
     return 0;
 }
